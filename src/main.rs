945fbc98b721cac945a7cfe1c03c437265aa4576
Bryan Hyland <bryan.hyland32@gmail.com>
2024-08-18 13:57:51 -0700

    Add comments to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-mod cli;
-mod logic;
+mod cli; // Add the CLI module
+mod logic; // Add module for the CLI tool logic
 
+// Import clap, cli, and logic stuff
 use clap::Parser;
 use cli::{Cli, Commands};
 use logic::{remove, setup};
 
 fn main() {
+    // Get the arguments given to the CLI
     let cli = Cli::parse();
     
+    // Match the commands
     match &cli.command {
+        // If it's the add command, parse the arguments and then do the logic
         Commands::Add { con_name, iface, ssid, hidden, auto_con } => setup(con_name.clone(), iface.clone(), ssid.clone(), *hidden, *auto_con),
+        // If it's remove, parse the argument and do the logic
         Commands::Remove { con_name } => remove(con_name.clone()),
     }
 }
